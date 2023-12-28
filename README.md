5ad74a0bf638046705f0a305a4f2910c8dab3a4f
Aylur <demstof@gmail.com>
2023-12-28 18:07:23 +0100

    oops accidentally force pushed

--- README.md
+++ README.md
@@ -8,3 +8,5 @@ npm run dev
 ## Sidebar
 
 To edit the side bar edit `astro.config.mjs`
+
+[aylur.github.io/ags-docs/](https://aylur.github.io/ags-docs/)
--- astro.config.mjs
+++ astro.config.mjs
@@ -49,7 +49,7 @@ export default defineConfig({
         starlight({
             title: 'AGS Wiki',
             editLink: {
-                baseUrl: 'https://github.com/Aylur/ags-docs',
+                baseUrl: 'https://github.com/Aylur/ags-docs/tree/main',
             },
             social: {
                 github: 'https://github.com/Aylur/ags',
